5d3c66bd266359880ab3782e4575dce8662b94fc
Sherry El Garhy <sherryptk@gmail.com>
2017-11-23 12:27:14 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,17 +1,9 @@
-require 'pry'
 class Patient
   attr_reader :name
 
   def initialize(name)
     @name = name
     @appointments = []
-  end
-
-  def add_appointment(appt)
-    binding.pry
-
-    @appointments << appt
-    appt.patient = self
   end
 
 end
